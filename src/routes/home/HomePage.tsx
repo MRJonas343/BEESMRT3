8352c40ece893d574bd27194cdf3114470bd9b58
Jonas <144829167+MRJonas343@users.noreply.github.com>
2024-04-23 10:39:55 -0500

    Update HomeCard component styles and grid layout in HomePage.tsx

--- src/routes/home/HomePage.tsx
+++ src/routes/home/HomePage.tsx
@@ -43,7 +43,7 @@ const HomePage: React.FC = () => {
       <NavBar />
 
       <h1 className="animate-tilt animate-duration-1000 animate-iteration-count-infinite  text-4xl text-center pb-3 text-white text-3d2 font-Principal tracking-wide from-neutral-400 md:text-4xl md:py-5 lg:text-6xl xl:py-6">BEESMRT GAMES</h1>
-      <section className="grid grid-cols-2 justify-items-center gap-4 h-[77vh] lg:flex lg:justify-center lg:items-center lg:gap-6 lg:h-[70vh]">
+      <section className="grid grid-cols-2 justify-items-center gap-4 h-[86dvh] lg:flex lg:justify-center lg:items-center lg:gap-6 lg:h-[70dvh]">
 
         <Link to="/games/MemoryGameInterface" className="flex">
           <HomeCard
@@ -66,7 +66,7 @@ const HomePage: React.FC = () => {
             text="Drag and drop the images to the correct place!!!" />
         </Link>
 
-        <div className="bg-white/70 p-5 rounded-2xl w-[43dvw] flex flex-col items-center justify-center overflow-hidden rounded-2x md:h-[37dvh] lg:w-[22dvw] lg:h-[50dvh]">
+        <div className="bg-white/70 p-5 rounded-2xl w-[43dvw] flex flex-col items-center justify-center overflow-hidden rounded-2x md:h-[37dvh] lg:w-[22dvw] lg:h-[55dvh]">
           <img src={ComingSoonImage} alt={""} className="w-32 lg:w-full" />
         </div>
 
