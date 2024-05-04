9ecf17fdc5bb74ae4bef6837235138ad420d2ecb
Jonas <144829167+MRJonas343@users.noreply.github.com>
2024-05-04 14:02:27 -0500

    Update package-lock.json and package.json with new dependency "@vercel/speed-insights" version 1.0.10, and fix API URLs in HomePage.tsx and Login.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,8 @@
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import { SpeedInsights } from '@vercel/speed-insights/next'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <>
     <App />
-    <SpeedInsights />
   </>
 )
