c9525fa5c7ea66daab016993d64bf0e8d5cb6e47
Jonas <144829167+MRJonas343@users.noreply.github.com>
2024-05-04 23:59:49 -0500

    Refactor routing in routes.tsx and lazy load route components

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,44 +1,48 @@
 import { Route, Routes, HashRouter } from 'react-router-dom'
+import { Suspense, lazy } from 'react'
 
 //* Components
+import Spinner from '@components/Spinner'
 import HomePage from '@routes/home/HomePage'
-import About from '@routes/about/About'
-import Contact from '@routes/contact/Contact'
-import NotFound from '@routes/notFound/NotFound'
-import LogIn from '@routes/login/Login'
-import SignUp from '@routes/signup/SignUp'
-import PrivacyPolicy from '@routes/privacypolicy/PrivacyPolicy'
-import MyAccount from '@routes/myAccount/MyAccount'
-import MemoryGameMode from '@routes/games/memoryGame/MemoryGameMode'
-import MemoryGame1vs1 from '@routes/games/memoryGame/MemoryGame1vs1'
-import HangmanGameMode from '@routes/games/hangmanGame/HangmanGameMode'
-import HangmanGameSingle from '@routes/games/hangmanGame/HangmanGameSingle'
-import DragDropGameMode from '@routes/games/dragDropGame/DragDropGameMode'
-import DragDropGameSingle from '@routes/games/dragDropGame/DragDropGameSingle'
+const About = lazy(() => import('@routes/about/About'))
+const Contact = lazy(() => import('@routes/contact/Contact'))
+const NotFound = lazy(() => import('@routes/notFound/NotFound'))
+const LogIn = lazy(() => import('@routes/login/Login'))
+const SignUp = lazy(() => import('@routes/signup/SignUp'))
+const PrivacyPolicy = lazy(() => import('@routes/privacypolicy/PrivacyPolicy'))
+const MyAccount = lazy(() => import('@routes/myAccount/MyAccount'))
+const MemoryGameMode = lazy(() => import('@routes/games/memoryGame/MemoryGameMode'))
+const MemoryGame1vs1 = lazy(() => import('@routes/games/memoryGame/MemoryGame1vs1'))
+const HangmanGameMode = lazy(() => import('@routes/games/hangmanGame/HangmanGameMode'))
+const HangmanGameSingle = lazy(() => import('@routes/games/hangmanGame/HangmanGameSingle'))
+const DragDropGameMode = lazy(() => import('@routes/games/dragDropGame/DragDropGameMode'))
+const DragDropGameSingle = lazy(() => import('@routes/games/dragDropGame/DragDropGameSingle'))
 
 
 const RoutesTree = () => {
   return (
-    <HashRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="*" element={<NotFound />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/login" element={<LogIn />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/privacypolicy" element={<PrivacyPolicy />} />
-        <Route path="/myaccount" element={<MyAccount />} />
+    <Suspense fallback={<div className='flex w-dvw h-dvh items-center justify-center'><Spinner /></div>}>
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="*" element={<NotFound />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/login" element={<LogIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/privacypolicy" element={<PrivacyPolicy />} />
+          <Route path="/myaccount" element={<MyAccount />} />
 
-        <Route path="/games/hangmangameMode" element={<HangmanGameMode />} />
-        <Route path="/games/memorygameMode" element={<MemoryGameMode />} />
-        <Route path="/games/dragdropgameMode" element={<DragDropGameMode />} />
+          <Route path="/games/hangmangameMode" element={<HangmanGameMode />} />
+          <Route path="/games/memorygameMode" element={<MemoryGameMode />} />
+          <Route path="/games/dragdropgameMode" element={<DragDropGameMode />} />
 
-        <Route path="/games/dragdropgameSingle" element={<DragDropGameSingle />} />
-        <Route path="/games/hangmangameSingle" element={<HangmanGameSingle />} />
-        <Route path="/games/memorygame1vs1" element={<MemoryGame1vs1 />} />
-      </Routes>
-    </HashRouter>
+          <Route path="/games/dragdropgameSingle" element={<DragDropGameSingle />} />
+          <Route path="/games/hangmangameSingle" element={<HangmanGameSingle />} />
+          <Route path="/games/memorygame1vs1" element={<MemoryGame1vs1 />} />
+        </Routes>
+      </HashRouter>
+    </Suspense>
   )
 }
 
