66fa1fab45b66f9010880cec8a28a69f4b70d200
Yam3101 <angelmay_810@hotmail.com>
2024-05-29 11:36:47 -0500

    changes on about us UI design

--- src/components/AboutCard.tsx
+++ src/components/AboutCard.tsx
@@ -7,7 +7,7 @@ const AboutCard: React.FC<AboutCardProps> = ({ image, title, text }) => {
 				<img src={image} alt={title} />
 			</div>
 			<div className="flex flex-col w-full">
-				<span className="font-Principal tracking-wide font-bold text-2xl py-2 text-center lg:text-left">
+				<span className="font-Principal tracking-wide font-normal text-2xl py-2 text-center lg:text-left">
 					{title}
 				</span>
 				<p className="font-Secundaria basis-[75%] lg:text-xl lg:pb-5">{text}</p>
