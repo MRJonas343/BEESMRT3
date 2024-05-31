66266f74bfc09da11a9a4537cd38ef78be7573ee
Yam3101 <angelmay_810@hotmail.com>
2024-05-31 14:00:58 -0500

    re edited progress dashboard

--- src/routes/myAccount/MyAccNew.tsx
+++ src/routes/myAccount/MyAccNew.tsx
@@ -6,25 +6,30 @@ function MyAccNew() {
 			<div className="bg-slate-900 py-5">
 				<NavBar />
 			</div>
-			<div className="flex flex-wrap justify-around mt-16 gap-5">
-				<div className="bg-white flex gap-5 p-8">
+			<div className="flex flex-wrap justify-around mt-16 gap-2">
+				<div className="bg-white flex gap-5 p-6 rounded-lg">
 					<img src="/defaultProfile.svg" alt="imgProfDef" />
 					<div className="grid">
 						<h1 className="font-semibold">BeeYam</h1>
 						<h2 className="">BeeName</h2>
 						<h2>Grade: Special</h2>
-						<div className="flex justify-center gap-3">
-							<div>Skills 1</div>
-							<div>Skills 2</div>
-							<div>Skills 3</div>
-						</div>
+						<h2>Skill Level: 1</h2>
 					</div>
 				</div>
-				<div className="bg-white flex gap-5 p-8 w-2/3">
-					<h1 className="font-bold">Advancements</h1>
+				<div className="bg-white grid p-8 w-3/4 rounded-lg">
+					<h1 className="font-bold m-0">Advancements</h1>
+					<div className="flex flex-wrap gap-7">
+						<img src="" alt="imgAd1" />
+						<img src="" alt="imgAd2" />
+						<img src="" alt="imgAd3" />
+						<img src="" alt="imgAd4" />
+						<img src="" alt="imgAd5" />
+						<img src="" alt="imgAd6" />
+						<img src="" alt="imgAd7" />
+					</div>
 				</div>
 			</div>
-			<div className="bg-white flex gap-5 p-8 m-10">
+			<div className="bg-white flex gap-5 p-8 m-10 rounded-lg">
 				<h1>Hola</h1>
 			</div>
 		</div>
