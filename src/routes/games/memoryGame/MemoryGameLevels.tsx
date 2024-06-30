600221046897745c8a9eb4014c356ed3ff9721db
Jonas <144829167+MRJonas343@users.noreply.github.com>
2024-06-30 14:52:41 -0500

    refactor: Include loading spinner in MemoryGameLevels component and fix API response handling

--- src/routes/games/memoryGame/MemoryGameLevels.tsx
+++ src/routes/games/memoryGame/MemoryGameLevels.tsx
@@ -7,8 +7,10 @@ import Trophy from "@assets/trophisIcon.png"
 import BasicModal from "@components/BasicModal"
 import { usePersonStore } from "../../../store/auth"
 import shyBee from "@assets/abeja-shy.webp"
+import Spinner from "@components/Spinner"
 
 const MemoryGameLevels = () => {
+	const [loading, setLoading] = useState(true)
 	const token = usePersonStore((state) => state.token)
 	const email = usePersonStore((state) => state.userEmail)
 	const [showModal, setShowModal] = useState(false)
@@ -65,6 +67,7 @@ const MemoryGameLevels = () => {
 				(level: EnglishLevel) => level.EnglishLevel === "A1",
 			)
 			setEnglishLevels(newLevels)
+			setLoading(!loading)
 			return
 		}
 
@@ -79,6 +82,7 @@ const MemoryGameLevels = () => {
 				(level: EnglishLevel) => level.EnglishLevel === "A1",
 			)
 			setEnglishLevels(newLevels)
+			setLoading(!loading)
 			return
 		}
 
@@ -88,6 +92,7 @@ const MemoryGameLevels = () => {
 			(level: EnglishLevel) => level.EnglishLevel === "A1",
 		)
 		setEnglishLevels(newLevels)
+		setLoading(!loading)
 	}
 
 	const changeLevels = (e: ChangeEvent<HTMLSelectElement>) => {
@@ -110,53 +115,61 @@ const MemoryGameLevels = () => {
 
 	return (
 		<div className="w-screen h-screen bg-Gradient2 overflow-x-hidden">
-			<NavBar />
-			<div className="bg-white m-6 p-10 pb-12 rounded-md text-center">
-				<div className="font-Secundaria text-3xl py-3">Memory Game Levels</div>
-				<div className="flex justify-center gap-4 items-center">
-					<h2 className="font-bold">English level :</h2>
-					<select
-						className="font-Principal text-xl px-4 rounded-lg border-black border-2"
-						onChange={changeLevels}
-					>
-						<option>A1</option>
-						<option>A2</option>
-						<option>B1</option>
-					</select>
-				</div>
-
-				<section className="flex flex-col w-auto gap-5">
-					<h2 className="text-center font-Secundaria text-2xl font-bold">
-						Choose a level
-					</h2>
-					<div className="flex gap-5 flex-wrap justify-center">
-						{englishLevels.map((level, index) => (
-							<div
-								key={index}
-								className="grid gap-1 text-center place-items-center"
+			{loading ? (
+				<Spinner />
+			) : (
+				<>
+					<NavBar />
+					<div className="bg-white m-6 p-10 pb-12 rounded-md text-center">
+						<div className="font-Secundaria text-3xl py-3">
+							Memory Game Levels
+						</div>
+						<div className="flex justify-center gap-4 items-center">
+							<h2 className="font-bold">English level :</h2>
+							<select
+								className="font-Principal text-xl px-4 rounded-lg border-black border-2"
+								onChange={changeLevels}
 							>
-								<button
-									key={index}
-									className="font-Secundaria hover:shadow-sm hover:shadow-slate-950 text-fuchsia-600 border-fuchsia-600 hover:text-amber-500 hover:border-amber-500 transition-btn-1 border-2 border-btn w-32 font-bold p-2 rounded-md"
-									onClick={redirectToGame}
-									type="button"
-								>
-									{level.LevelName}
-								</button>
-								<h3 className="font-semibold">{level.Trophys}</h3>
-								<img src={Trophy} alt="Trophy" className="w-10" />
+								<option>A1</option>
+								<option>A2</option>
+								<option>B1</option>
+							</select>
+						</div>
+
+						<section className="flex flex-col w-auto gap-5">
+							<h2 className="text-center font-Secundaria text-2xl font-bold">
+								Choose a level
+							</h2>
+							<div className="flex gap-5 flex-wrap justify-center">
+								{englishLevels.map((level, index) => (
+									<div
+										key={index}
+										className="grid gap-1 text-center place-items-center"
+									>
+										<button
+											key={index}
+											className="font-Secundaria hover:shadow-sm hover:shadow-slate-950 text-fuchsia-600 border-fuchsia-600 hover:text-amber-500 hover:border-amber-500 transition-btn-1 border-2 border-btn w-32 font-bold p-2 rounded-md"
+											onClick={redirectToGame}
+											type="button"
+										>
+											{level.LevelName}
+										</button>
+										<h3 className="font-semibold">{level.Trophys}</h3>
+										<img src={Trophy} alt="Trophy" className="w-10" />
+									</div>
+								))}
 							</div>
-						))}
+						</section>
 					</div>
-				</section>
-			</div>
-			<BasicModal
-				showModal={showModal}
-				closeModal={() => setShowModal(!showModal)}
-				imageSrc={imageSrc}
-				message={message}
-				mainMessage={mainMessage}
-			/>
+					<BasicModal
+						showModal={showModal}
+						closeModal={() => setShowModal(!showModal)}
+						imageSrc={imageSrc}
+						message={message}
+						mainMessage={mainMessage}
+					/>
+				</>
+			)}
 		</div>
 	)
 }
