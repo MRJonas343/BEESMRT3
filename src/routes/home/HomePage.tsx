77c182881a06644348fac3ba2d01c2c60018a2c0
Jonas <144829167+MRJonas343@users.noreply.github.com>
2024-07-01 10:19:15 -0500

    refactor: Update text shadow styles and background gradient in homepage

--- src/routes/home/HomePage.tsx
+++ src/routes/home/HomePage.tsx
@@ -12,10 +12,10 @@ import ComingSoonImage from "@assets/comingSoon.webp"
 
 const HomePage: React.FC = () => {
 	return (
-		<main className="w-screen h-screen bg-Gradient2 overflow-x-hidden">
+		<main className="w-screen h-screen bg-Gradient1 overflow-x-hidden">
 			<NavBar />
 
-			<h1 className="animate-tilt animate-duration-1000 animate-iteration-count-infinite  text-4xl text-center pb-3 text-white text-3d2 font-Principal tracking-wide from-neutral-400 md:text-4xl md:py-5 lg:text-6xl xl:py-6">
+			<h1 className="animate-tilt tracking-wide animate-duration-1000 animate-iteration-count-infinite  text-4xl text-center pb-3 text-Yellow1 text-3d3 font-Principal from-neutral-400 md:text-4xl md:py-5 lg:text-6xl xl:py-6">
 				BEESMRT GAMES
 			</h1>
 			<section className="grid grid-cols-2 justify-items-center gap-4 h-[84dvh] md:h-[82dvh] lg:flex lg:justify-center lg:items-center lg:gap-4 lg:h-[70dvh]">
