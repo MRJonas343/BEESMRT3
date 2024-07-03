b8974042dc7c168b3e2f3454dab564b4d9c300de
Jonas <144829167+MRJonas343@users.noreply.github.com>
2024-07-03 18:26:48 -0500

    refactor: Update HomeCard component with image source, title, and text props

--- src/components/HomeCard.tsx
+++ src/components/HomeCard.tsx
@@ -1,5 +1,12 @@
 import { HomeCardProps } from "@types"
 
+/**
+ * Represents a home card component.
+ * @param imageSrc - The source of the image.
+ * @param title - The title of the card.
+ * @param text - The text content of the card.
+ */
+
 const HomeCard: React.FC<HomeCardProps> = ({ imageSrc, title, text }) => {
 	return (
 		<div className="bg-white/70 w-[43vw] h-[40vh] flex flex-col items-center overflow-auto p-5 rounded-2xl ease-in-out duration-200 hover:scale-105 hover:bg-yellow-400 cursor-pointer md:h-[37dvh] lg:w-[22dvw] lg:h-[55dvh] lg:pt-20 shadow-2xl">
