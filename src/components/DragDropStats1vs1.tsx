ec6b0a3d214cabf0f8360aff6ecc4cb14a88f73d
MrJonas343 <angeljonasrosales@gmail.com>
2024-07-11 17:11:45 -0500

    refactor: Update DragDropStats1vs1 component with responsive styling for player images and points

--- src/components/DragDropStats1vs1.tsx
+++ src/components/DragDropStats1vs1.tsx
@@ -10,7 +10,7 @@ const DragDrop1vs1Stats: React.FC<DragDrop1vs1StatsProps> = ({
 	player1Points,
 	player2Points,
 }) => {
-	const baseClassName = "w-14 h-14 rounded-full"
+	const baseClassName = "w-14 h-14 rounded-full lg:w-32 lg:h-32"
 	const activePlayerClassName = `${baseClassName} drop-shadow-lg outline outline-Pink1 outline-4`
 	const inactivePlayerClassName = `${baseClassName}`
 
@@ -23,21 +23,23 @@ const DragDrop1vs1Stats: React.FC<DragDrop1vs1StatsProps> = ({
 	return (
 		<>
 			<div className="pt-4 pb-1 flex justify-between flex-row w-full">
-				<div className="flex w-4/6 justify-center gap-6 px-8 items-center">
+				<div className="flex w-4/6 justify-center gap-6 px-8 items-center ">
 					<div>
 						<img src={RedBee} alt="Red Bee" className={player1ClassName} />
 					</div>
-					<p className="text-2xl font-Principal text-Yellow1 text-3d">VS</p>
+					<p className="text-3xl font-Principal text-Yellow1 text-3d lg:text-5xl">
+						VS
+					</p>
 					<div>
 						<img src={BlueBee} alt="Red Bee" className={player2ClassName} />
 					</div>
 				</div>
 
 				<div className="w-2/6 flex flex-col mr-6">
-					<p className="text-lg font-Principal text-red-400 text-3d">
+					<p className="text-xl font-Principal text-red-400 text-3d lg:text-3xl">
 						Player 1 points : {player1Points}
 					</p>
-					<p className="text-lg font-Principal text-blue-400 text-3d">
+					<p className="text-xl font-Principal text-blue-400 text-3d lg:text-3xl">
 						Player 2 points : {player2Points}
 					</p>
 				</div>
