fa67422a368fbc56624a3565933ba4f58ce799a0
Yam3101 <angelmay_810@hotmail.com>
2024-07-18 17:35:41 -0500

    finished scoreboard

--- src/routes/myAccount/Scoreboard.tsx
+++ src/routes/myAccount/Scoreboard.tsx
@@ -5,16 +5,28 @@ function Scoreboard() {
 	return (
 		<div className="bg-rose-600 w-screen h-screen overflow-x-hidden">
 			<NavBar />
-			<div className="flex flex-col justify-center items-center gap-5">
+			<div className="flex flex-col justify-center items-center gap-5 pb-10">
 				<h1 className="bg-white font-Principal text-4xl font-semibold shadow-rose-950 shadow-lg py-4 px-10 rounded-md mt-7">
 					TOP Best Month Users
 				</h1>
 				<div className="bg-white rounded-md shadow-lg p-5 w-4/5">
+					<div className="flex justify-between pb-4 border-b-2 border-gray-200">
+						<div className="text-3xl font-Principal">Top Bee Users</div>
+						<div className="text-3xl font-Principal">Trophies</div>
+					</div>
 					<ul className="divide-y divide-gray-200">
 						{usuariosBee.map((user, index) => (
 							<li
 								key={index}
-								className="py-4 flex items-center justify-between"
+								className={`py-4 flex items-center justify-between ${
+									index === 0
+										? "text-violet-900"
+										: index === 1
+											? "text-rose-800"
+											: index === 2
+												? "text-amber-700"
+												: ""
+								}`}
 							>
 								<div className="flex items-center">
 									<img
@@ -23,14 +35,14 @@ function Scoreboard() {
 										alt={`${user.nickName}'s profile`}
 									/>
 									<div className="ml-4">
-										<h2 className="text-xl font-Principal">{user.nickName}</h2>
+										<h2 className="text-2xl font-Principal">{user.nickName}</h2>
 										<div className="text-sm text-gray-500">
 											English Level: {user.englishLevel || "N/A"}
 										</div>
 									</div>
 								</div>
 								<div className="text-lg font-semibold">
-									Trophies: {user.TotalTrophies}
+									{user.TotalTrophies}
 								</div>
 							</li>
 						))}
